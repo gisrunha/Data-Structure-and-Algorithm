6f91853e6cd1fbbd12cb38a1386524e7106c23b1
Gisrunha <gisrunha@gmail.com>
2025-02-04 21:44:53 +0600

    refactor: improve code readability in findPeakElement implementation

--- Leetcode/Medium/0162_FindPeakElement.cpp
+++ Leetcode/Medium/0162_FindPeakElement.cpp
@@ -24,11 +24,11 @@ int findPeakElement(vector<int> &nums)
     {
         int mid = left + (right - left) / 2;
 
-        if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
         {
             return mid;
         }
-        else if (nums[mid-1] < nums[mid])
+        else if (nums[mid - 1] < nums[mid])
         {
             if (mid == s - 2)
             {
@@ -39,9 +39,9 @@ int findPeakElement(vector<int> &nums)
                 left = mid + 1;
             }
         }
-        else if (nums[mid] > nums[mid+1])
+        else if (nums[mid] > nums[mid + 1])
         {
-            if(mid == 1)
+            if (mid == 1)
             {
                 return 0;
             }
@@ -52,7 +52,7 @@ int findPeakElement(vector<int> &nums)
         }
         else
         {
-            if(s == 3)
+            if (s == 3)
             {
                 return 0;
             }
@@ -67,6 +67,6 @@ int findPeakElement(vector<int> &nums)
 
 int main(void)
 {
-    vector<int> arr = {1,2,3,1};
+    vector<int> arr = {1, 2, 3, 1};
     cout << findPeakElement(arr);
 }
